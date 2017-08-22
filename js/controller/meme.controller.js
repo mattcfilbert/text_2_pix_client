183fb037963432cdabc4f7c8a1ea22d4a7dea266
MATT FILBERT <mcfilbert@email.wm.edu>
2017-08-22 13:43:29 -0400

    hopefull merged

--- js/controller/meme.controller.js
+++ js/controller/meme.controller.js
@@ -15,8 +15,6 @@
     'Meme',
     'Comment',
     'Photo',
-    'Text',
-    'Cat',
     MemeShowControllerFunction
   ])
   function MemeIndexControllerFunction ($state, Meme, Photo) {
@@ -26,21 +24,19 @@
     this.newMeme = new Meme()
 
     this.create = function () {
-      this.randomPhotoUrl = this.createPhoto.data.memes[Math.floor(Math.random() * this.createPhoto.data.memes.length)].url
+      this.randomPhotoUrl = this.createPhoto.images[Math.floor(Math.random() * this.createPhoto.images.length)].url
       this.newMeme.img_url = this.randomPhotoUrl
       this.newMeme.$save((newMeme) => {
         $state.go('memeShow', {id: newMeme.id})
       })
     }
   }
-  function MemeShowControllerFunction ($window, $state, $stateParams, Meme, Comment, Photo, Text, Cat) {
+  function MemeShowControllerFunction ($window, $state, $stateParams, Meme, Comment, Photo) {
     this.meme = Meme.get({ id: $stateParams.id })
     this.meme.comments = Meme.get({ id: $stateParams.id }).comments
     this.newComment = new Comment()
-    this.funnyText = Text.get()
-    this.photo = Photo.get()
-    this.newCat = Cat.get()
 
+    this.photo = Photo.get()
     this.createComment = function () {
       this.newComment.meme_id = this.meme.id
       this.newComment.$save(() => {
@@ -73,28 +69,8 @@
       })
     }
     this.newPhoto = function () {
-      this.randomPhotoUrl = this.photo.data.memes[Math.floor(Math.random() * this.photo.data.memes.length)].url
-      console.log(this.photo.data)
+      this.randomPhotoUrl = this.photo.images[Math.floor(Math.random() * this.photo.images.length)].url
       this.meme.img_url = this.randomPhotoUrl
-      this.meme.$update({ id: $state.params.id })
-      // $window.location.reload()
-    }
-    this.newText = function () {
-      console.log(this.funnyText.attachments[0].fallback)
-      this.newText = this.funnyText.attachments[Math.floor(Math.random() * this.funnyText.attachments.length)].fallback
-      this.meme.text = this.newText
-      this.meme.$update({ id: $state.params.id })
-      $window.location.reload()
-    }
-    this.catPicture = function () {
-      this.meme.img_url = this.newCat.file
-      console.log(this.newCat.file)
-      this.meme.$update({ id: $state.params.id })
-
-      $window.location.reload()
-    }
-    this.dropPhoto = function () {
-      this.meme.img_url = this.dropDownPhoto.url
       this.meme.$update({ id: $state.params.id })
       $window.location.reload()
     }
