5be973f8a1316f28d7aa2204d49d90c7c7277b45
Jacob Greenwald <jeg4t@virginia.edu>
2017-08-22 11:57:14 -0400

    add save when selecting meme with dropdown

--- js/controller/meme.controller.js
+++ js/controller/meme.controller.js
@@ -95,6 +95,8 @@
     }
     this.dropPhoto = function () {
       this.meme.img_url = this.dropDownPhoto.url
+      this.meme.$update({ id: $state.params.id })
+      $window.location.reload()
     }
   }
 })()
