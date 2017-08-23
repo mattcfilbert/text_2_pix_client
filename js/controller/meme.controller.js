d887bcd0735d621202f29840e3f988acac357c8e
MATT FILBERT <mcfilbert@email.wm.edu>
2017-08-23 13:58:52 -0400

    working user session ish

--- js/controller/meme.controller.js
+++ js/controller/meme.controller.js
@@ -24,10 +24,22 @@
     this.photo = Photo.get()
     this.createPhoto = Photo.get()
     this.newMeme = new Meme()
+    this.yourMemes = JSON.parse(window.sessionStorage.getItem('savedMemes')) || []
+
+    this.addMeme = function (newMeme) {
+      var existingMemes = JSON.parse(window.sessionStorage.getItem('savedMemes')) || []
+      existingMemes.push(newMeme)
+      this.yourMemes = existingMemes
+      window.sessionStorage.setItem('savedMemes', JSON.stringify(existingMemes))
+    }
 
     this.create = function () {
       this.randomPhotoUrl = this.createPhoto.data.memes[Math.floor(Math.random() * this.createPhoto.data.memes.length)].url
       this.newMeme.img_url = this.randomPhotoUrl
+      this.addMeme(this.newMeme)
+      // this.storedMemes = this.storedMemes + `${this.newMeme}`
+      // localStorage.setItem('memes', JSON.stringify(this.storedMemes))
+
       this.newMeme.$save((newMeme) => {
         $state.go('memeShow', {id: newMeme.id})
       })
@@ -76,14 +88,15 @@
       console.log(this.photo.data)
       this.meme.img_url = this.randomPhotoUrl
       this.meme.$update({ id: $state.params.id })
-      $window.location.reload()
+      // $window.location.reload()
     }
     this.newText = function () {
       console.log(this.funnyText.attachments[0].fallback)
+      this.funnyText = Text.get()
       this.newText = this.funnyText.attachments[Math.floor(Math.random() * this.funnyText.attachments.length)].fallback
       this.meme.text = this.newText
       this.meme.$update({ id: $state.params.id })
-      $window.location.reload()
+      // $window.location.reload()
     }
     this.catPicture = function () {
       this.meme.img_url = this.newCat.file
